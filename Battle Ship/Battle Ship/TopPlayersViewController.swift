7357be62e66083a1e1ab4bdf97dbb9003b3e7f6d
Dumidu <dumidusumanasekara@gmail.com>
2022-04-16 13:02:38 -0400

    inititaed new view for top players

---
+++ Battle Ship/Battle Ship/TopPlayersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TopPlayersViewController.swift
+//  Battle Ship
+//
+//  Created by Dumidu Sumanasekara on 2022-04-16.
+//
+
+import UIKit
+
+class TopPlayersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
